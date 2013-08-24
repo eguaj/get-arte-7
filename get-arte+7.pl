c6de8bbee101102cf59eda953771eb6e9517a1da
Jérôme Augé <jerome.auge@gmail.com>
2013-08-24 23:37:09 +0200

    Updated after Arte+7 site update
    The stream is now directly available in .mp4 format.

--- get-arte+7.pl
+++ get-arte+7.pl
@@ -3,12 +3,6 @@
 use strict;
 use warnings;
 use LWP::UserAgent;
-use Getopt::Long;
-
-my $convert_to_mp4 = undef;
-my $result = GetOptions(
-    "mp4" => \$convert_to_mp4
-    );
 
 my $page_url = shift;
 my $outFile = shift;
@@ -24,7 +18,7 @@ if( not defined $page_url ) {
 if( not defined $outFile ) {
     ($outFile) = ($page_url =~ m|.*/([^/]+)$|);
     $outFile =~ s/\.[a-z0-9]+$//i;
-    $outFile = sprintf("%s.flv", $outFile);
+    $outFile = sprintf("%s.mp4", $outFile);
 }
 
 my $agent = new LWP::UserAgent();
@@ -39,72 +33,34 @@ if( ! $response->is_success ) {
 }
 $content = $response->content;
 
-my ($videorefFileUrl) = ($content =~ m|^\s*vars_player\.videorefFileUrl\s*=\s*"([^"]+)"\s*;\s*$|ms);
-
-if( not defined $videorefFileUrl ) {
-    print STDERR sprintf("Could not find videorefFileUrl!\n");
+my ($arte_vp_url) = ($content =~ m|arte_vp_url="([^"]+)"|ms);
+if( not defined $arte_vp_url ) {
+    print STDERR sprintf("Could not find arte_vp_url!\n");
     exit( 2 );
 }
 
-print STDERR sprintf("videorefFileUrl = %s\n", $videorefFileUrl);
-
-$response = $agent->get($videorefFileUrl);
+$response = $agent->get($arte_vp_url);
 if( ! $response->is_success ) {
-    print STDERR sprintf("Error fetching videorefFileUrl '%s': %s\n", $videorefFileUrl, $response->status_line);
+    print STDERR sprintf("Error fetching arte_vp_url '%s': %s\n", $arte_vp_url, $response->status_line);
     exit( 3 );
 }
 $content = $response->content;
 
-my ($videoFrUrl) = ($content =~ m|<video\s+lang="fr"\s+ref="([^"]+)"\s*/>|ms);
-if( not defined $videoFrUrl ) {
-    print STDERR sprintf("Could not find videoFrUrl!\n");
+my ($url) = ($content =~ m|HTTP_REACH_EQ_1":.*?"url":"([^"]+)"|ms);
+if( not defined $url ) {
+    print STDERR sprintf("Could not find 'HTTP_REACH_EQ_1' URL!\n");
     exit( 4 );
 }
-
-print STDERR sprintf("videoFrUrl = '%s'\n", $videoFrUrl);
-
-$response = $agent->get($videoFrUrl);
-if( ! $response->is_success ) {
-    print STDERR sprintf("Error fetching '%s': %s", $videoFrUrl, $response->status_line);
-    exit( 5 );
-}
-$content = $response->content;
-
-my ($videoUrl) = ($content =~ m|<url\s+quality="hd"\s*>\s*(rtmp://[^<]+)</url>|ms);
-if( not defined $videoUrl ) {
-    print STDERR sprintf("Could not find videoUrl!\n");
-    exit( 6 );
-}
-
-print STDERR sprintf("Fetching videoUrl '%s' into file '%s'\n", $videoUrl, $outFile);
-
-my @cmd = ('rtmpdump', '-r', $videoUrl, '-o', $outFile, '-e');
-my $ret = 2;
-while( $ret == 2 ) {
-    system(@cmd);
-    $ret = $?;
-    if( $ret == -1 ) {
-	print STDERR sprintf("Could not execute, or find, the 'rtmpdump' command!\n");
-	exit(1);
-    }
+my @cmd = ('wget', '-O', $outFile, '-c', $url);
+print STDERR sprintf("Fetching URL '%s' into file '%s'\n", $url, $outFile);
+system(@cmd);
+my $ret = $?;
+if( $ret == -1 ) {
+    print STDERR sprintf("Could not execute, or find, the 'wget' command\n");
+    exit(1);
 }
 
 if( $ret != 0 ) {
-	print STDERR sprintf("rtmpdump returned with error code '%s'\n", $ret);
-	exit( $ret );
+    print STDERR sprintf("'%s' returned with error code '%s'\n", join(' ', @cmd), $ret);
 }
-
-if( $convert_to_mp4 ) {
-    (my $outFileMP4 = $outFile) =~ s/\.flv$/.mp4/;
-    @cmd = ('ffmpeg', '-i', $outFile, '-acodec', 'copy', '-vcodec', 'copy', '-f', 'mp4', $outFileMP4);
-    system(@cmd);
-    $ret = $?;
-    if( $ret != 0) {
-	print STDERR sprintf("Could not convert '%s' to '%s'!\n", $outFile, $outFileMP4);
-	unlink($outFile);
-	exit($ret);
-    }
-    unlink($outFile);
-}
-
-exit(0);
+exit( $ret );
